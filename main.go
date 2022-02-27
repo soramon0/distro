b8d4328bd5bcdb7da012fd6a0f8a2eb5fd021e90
sormaon0 <karimacc001@gmail.com>
2022-02-27 15:45:39 +0100

    Fix Recipe data type

--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
 
 type Recipe struct {
-	Id           string   `json:"id"`
+	ID           string   `json:"id"`
 	Name         string   `json:"name"`
 	Tags         []string `json:"tags"`
 	Ingredients  []string `json:"ingredients"`
 	Instructions []string `json:"instructions"`
-	PublishedAt  []string `json:"publishedAt"`
+	PublishedAt  time.Time `json:"publishedAt"`
 }
 
 func main() {
